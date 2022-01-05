48b28cbea2ae62281f4bc2aa31cf59f8ffa40233
Louis C-H <louisclementharris@yahoo.co.uk>
2022-01-05 12:35:00 +0000

    Added tests to RandomNumberGenerator, and added a contingency for if lower bound > upper bound

--- src/main/java/model/RandomNumberGenerator.java
+++ src/main/java/model/RandomNumberGenerator.java
@@ -5,6 +5,12 @@ import java.util.Random;
 public class RandomNumberGenerator {
 
     public static int getRandomInt(int lowerBound, int upperBound){
+        // Note: possible generated ints include lowerBound and upperBound.
+        if (lowerBound > upperBound){
+            int storageInt = upperBound;
+            upperBound = lowerBound;
+            lowerBound = storageInt;
+        }
         Random randomGenerator = new Random();
         int randomInt = randomGenerator.nextInt(upperBound - lowerBound + 1) + lowerBound;
         return randomInt;
