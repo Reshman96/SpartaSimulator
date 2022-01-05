f271785088bb53315e2e9c4ee472c827ac2535f5
Harry Jones <harryrjones@yahoo.com>
2022-01-05 12:21:54 +0000

    Testing Centre

--- src/test/java/model/CentreTests.java
+++ src/test/java/model/CentreTests.java
@@ -1,10 +1,16 @@
 package model;
 
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CentreTests {
 
-    private Centre centre;
+    static Centre centreDefault;
+    static Centre centreCustom;
 
     //When a centre is made, make sure number of trainees is 0
     //Has a max capacity of 100
@@ -12,14 +18,42 @@ public class CentreTests {
     //Check isFull boolean returns true when true
     //Check isFull boolean returns false when false
     //Test negative number of trainees - add method???
-    //Test negative number for max size
-    //Test negative number for capacity
 
-    @Test
-    public void testBaseConstructor(){
+    @BeforeAll
+    static void setup(){
+         centreDefault = new Centre();
+         centreCustom = new Centre(250);
 
+         centreDefault.setNumberOfTrainees(100);
     }
 
+    @Test
+    @DisplayName("Test Trainees equals 0")
+    void testTraineesEquals0() {
+        assertEquals(0, centreDefault.getNumberOfTrainees());
+    }
 
+    @Test
+    @DisplayName("TestDefaultMaxCapacity100")
+    void testDefaultMaxCapacity100() {
+        assertEquals(100, centreDefault.getMaxSize());
+    }
 
+    @Test
+    @DisplayName("Test Initial monthly capacity is 0")
+    void testInitialMonthlyCapacityIs0() {
+        assertEquals(250, centreCustom.getMaxSize());
+    }
+
+    @Test
+    @DisplayName("Test isFull for true")
+    void testIsFullForTrue() {
+        assertEquals(true, centreDefault.isFull());
+    }
+
+    @Test
+    @DisplayName("Test isFull for false")
+    void testIsFullForFalse() {
+        assertEquals(false, centreCustom.isFull());
+    }
 }
