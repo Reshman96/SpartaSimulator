8124c7fc6f5fc7ef1dbbfd42ca323f308f580aa7
Pavle <pavlesvrdlin@outlook.com>
2022-01-05 15:22:29 +0000

    getters and setters included

--- src/main/java/controller/SpartaSimulator.java
+++ src/main/java/controller/SpartaSimulator.java
@@ -1,4 +1,73 @@
 package controller;
 
 public class SpartaSimulator {
+    private MonthlyIncrementor monthlyIncrementor = new MonthlyIncrementor();
+    //private CentreManager centreManager = new CentreManager();
+
+    private int months;
+    private int numOfOpenCentres;
+    private int numOfFullCentres;
+    private int numOfTraineesTraining;
+    private int numOfTraineesWaiting;
+
+    public SpartaSimulator(int months, int numOfOpenCentres, int numOfFullCentres, int numOfTraineesTraining, int numOfTraineesWaiting) {
+        this.months = months;
+        this.numOfOpenCentres = numOfOpenCentres;
+        this.numOfFullCentres = numOfFullCentres;
+        this.numOfTraineesTraining = numOfTraineesTraining;
+        this.numOfTraineesWaiting = numOfTraineesWaiting;
+    }
+
+    public SpartaSimulator(int months) {
+        this.months = months;
+        this.numOfOpenCentres = 0;
+        this.numOfFullCentres = 0;
+        this.numOfTraineesTraining = 0;
+        this.numOfTraineesWaiting = 0;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+    public void setMonths(int months) {
+        this.months = months;
+    }
+
+    public int getNumOfOpenCentres() {
+        return numOfOpenCentres;
+    }
+
+    public void setNumOfOpenCentres(int numOfOpenCentres) {
+        this.numOfOpenCentres = numOfOpenCentres;
+    }
+
+    public int getNumOfFullCentres() {
+
+        return numOfFullCentres;
+    }
+
+    public void setNumOfFullCentres(int numOfFullCentres) {
+        this.numOfFullCentres = numOfFullCentres;
+    }
+
+    public int getNumOfTraineesTraining() {
+        return numOfTraineesTraining;
+    }
+
+    public void setNumOfTraineesTraining(int numOfTraineesTraining) {
+        this.numOfTraineesTraining = numOfTraineesTraining;
+    }
+
+    public int getNumOfTraineesWaiting() {
+        return numOfTraineesWaiting;
+    }
+
+    public void setNumOfTraineesWaiting(int numOfTraineesWaiting) {
+        this.numOfTraineesWaiting = numOfTraineesWaiting;
+    }
+
+    public void incrementMonth(){
+
+    }
 }
