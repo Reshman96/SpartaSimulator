68a2548cdfa931b284592446ed20f8d88f3c204f
Michael <michael_makam18@hotmail.com>
2022-01-05 12:28:45 +0000

    FileOutputManager updated file.

--- src/main/java/view/FileOutputManager.java
+++ src/main/java/view/FileOutputManager.java
@@ -9,6 +9,7 @@ public class FileOutputManager {
     private static void openCSVFile() {
         try {
             bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/csv_file_example.csv"));
+            bufferedWriter.write("Month,Full_centres,Open_centres,Trainees_Training,Trainees_waiting\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -16,9 +17,7 @@ public class FileOutputManager {
 
     private static void writeDetailsToFile(int month, int traineesInTraining, int traineesWaiting, int openCentres, int fullCentres) {
         try {
-            bufferedWriter.write("Month,Full_centres,Open_centres,Trainees_Training,Trainees_waiting\n");
             bufferedWriter.write(month + "," + fullCentres + "," + openCentres + "," + traineesInTraining + "," +traineesWaiting + "\n");
-
         } catch (IOException e) {
             e.printStackTrace();
         }
