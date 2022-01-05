ba19d53de1c2e1296ddc9c1fa04aaef8ef701f9a
TWParsons <TParsons@SpartaGlobal.com>
2022-01-05 11:41:31 +0000

    Test class for centre class

---
+++ src/test/java/model/CentreTests.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.junit.Test;
+
+public class CentreTests {
+
+    private Centre centre;
+
+    //When a centre is made, make sure number of trainees is 0
+    //Has a max capacity of 100
+    //Initial Monthly Capacity is 0
+    //Check isFull boolean returns true when true
+    //Check isFull boolean returns false when false
+    //Test negative number of trainees - add method???
+    //Test negative number for max size
+    //Test negative number for capacity
+
+    @Test
+    public void testBaseConstructor(){
+
+    }
+
+
+
+}
