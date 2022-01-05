e764d97671d4cbd02f4d79d467d29a264de90597
JamesDorling <jamesrdorling@gmail.com>
2022-01-05 11:38:41 +0000

    Made output printer final and have static methods

--- src/main/java/view/OutputPrinter.java
+++ src/main/java/view/OutputPrinter.java
@@ -1,26 +1,26 @@
 package view;
 
-public class OutputPrinter {
-    public void printMonthNumber(int currentMonth) {
+public final class OutputPrinter {
+    public static void printMonthNumber(int currentMonth) {
         System.out.println("Current month: " + currentMonth);
     }
-    public void printFullCentres(int fullCentres) {
+    public static void printFullCentres(int fullCentres) {
         System.out.println("Number of full centres: " + fullCentres);
     }
 
-    public void printOpenCentres(int openCentres) {
+    public static void printOpenCentres(int openCentres) {
         System.out.println("Number of open centres: " + openCentres);
     }
 
-    public void printCurrentTrainees(int currentTrainees) {
+    public static void printCurrentTrainees(int currentTrainees) {
         System.out.println("Number of current trainees being trained: " + currentTrainees);
     }
 
-    public void printWaitingList(int waitingListSize) {
+    public static void printWaitingList(int waitingListSize) {
         System.out.println("Number of trainees on the waiting list: " + waitingListSize);
     }
 
-    public void printMonthlyData(int currentMonth, int fullCentres, int openCentres, int currentTrainees, int waitingListSize) {
+    public static void printMonthlyData(int currentMonth, int fullCentres, int openCentres, int currentTrainees, int waitingListSize) {
         printMonthNumber(currentMonth);
         printFullCentres(fullCentres);
         printOpenCentres(openCentres);
