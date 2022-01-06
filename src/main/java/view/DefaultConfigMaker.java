63bc2e7f1b80340873f161552b11f405f73faa21
resh <reshmansiva96@gmail.com>
2022-01-06 12:06:04 +0000

    updating DefaultConfigMaker to include the additional values

--- src/main/java/view/DefaultConfigMaker.java
+++ src/main/java/view/DefaultConfigMaker.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
@@ -10,11 +9,28 @@ public class DefaultConfigMaker {
     public static void createDefaultConfig() {
         Properties properties = new Properties();
         try {
-            properties.setProperty("centresPerMonth", "0.5");
-            properties.setProperty("traineeUpperBound", "100");
             properties.setProperty("traineeLowerBound", "50");
+            properties.setProperty("traineeUpperBound", "100");
+            properties.setProperty("centresPerMonth", "0.5");
             properties.setProperty("monthsOfSimulation", "24");
             properties.setProperty("existingCentres", "0");
+            properties.setProperty("displayEveryMonth", "false");
+
+            properties.setProperty("trainingHubMinimumTrainees", "25");
+            properties.setProperty("trainingHubMaximumTrainees", "100");
+            properties.setProperty("trainingHubConsecutiveMonthsOfInadequateTrainees", "1");
+            properties.setProperty("trainingHubsPerMonth", "3");
+
+            properties.setProperty("bootcampMinimumTrainees", "25");
+            properties.setProperty("bootcampMaximumTrainees", "500");
+            properties.setProperty("bootcampConsecutiveMonthsOfInadequateTrainees", "3");
+            properties.setProperty("bootcampTotalCreations", "2");
+
+            properties.setProperty("techCentreMinimumTrainees", "25");
+            properties.setProperty("techCentreMaximumTrainees", "200");
+            properties.setProperty("techCentresConsecutiveMonthsOfInadequateTrainees", "1");
+
+
             properties.store(new FileWriter("src/main/resources/config.properties"), null);
 
         } catch (IOException e) {
