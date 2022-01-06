7c170af0dd56a9332cec38e6e82303c1ebeb7ef4
Alexander Jarrett <alexjarrett400@gmail.com>
2022-01-06 12:28:42 +0000

    Refactoring OutputManager to work with user choice Prepping OutputPrinter for new data

--- src/main/java/view/OutputPrinter.java
+++ src/main/java/view/OutputPrinter.java
@@ -6,18 +6,53 @@ public final class OutputPrinter {
     }
     private static void printFullCentres(int fullCentres) {
         System.out.println("Number of full centres: " + fullCentres);
+        System.out.println("Training hubs: ");
+        System.out.println("Bootcamps: ");
+        System.out.println("Java Tech Centres: ");
+        System.out.println("C# Tech Centres: ");
+        System.out.println("Data Tech Centres: ");
+        System.out.println("DevOps Tech Centres: ");
+        System.out.println("Business Tech Centres: ");
     }
 
     private static void printOpenCentres(int openCentres) {
         System.out.println("Number of open centres: " + openCentres);
+        System.out.println("Training hubs: ");
+        System.out.println("Bootcamps: ");
+        System.out.println("Java Tech Centres: ");
+        System.out.println("C# Tech Centres: ");
+        System.out.println("Data Tech Centres: ");
+        System.out.println("DevOps Tech Centres: ");
+        System.out.println("Business Tech Centres: ");
+    }
+
+    private static void printClosedCentres(int closedCentres){
+        System.out.println("Number of open centres: " + closedCentres);
+        System.out.println("Training hubs: ");
+        System.out.println("Bootcamps: ");
+        System.out.println("Java Tech Centres: ");
+        System.out.println("C# Tech Centres: ");
+        System.out.println("Data Tech Centres: ");
+        System.out.println("DevOps Tech Centres: ");
+        System.out.println("Business Tech Centres: ");
     }
 
     private static void printCurrentTrainees(int currentTrainees) {
         System.out.println("Number of current trainees being trained: " + currentTrainees);
+        System.out.println("Java Trainees: ");
+        System.out.println("C# Trainees: ");
+        System.out.println("Data Trainees: ");
+        System.out.println("DevOps Trainees: ");
+        System.out.println("Business Trainees: ");
     }
 
     private static void printWaitingList(int waitingListSize) {
         System.out.println("Number of trainees on the waiting list: " + waitingListSize);
+        System.out.println("Java Trainees: ");
+        System.out.println("C# Trainees: ");
+        System.out.println("Data Trainees: ");
+        System.out.println("DevOps Trainees: ");
+        System.out.println("Business Trainees: ");
     }
 
     public static void printMonthlyData(int currentMonth, int fullCentres, int openCentres, int currentTrainees, int waitingListSize) {
