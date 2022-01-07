86a5676a187018d5b84160ac7ea2dbb6c2d8aa6b
Pavle <pavlesvrdlin@outlook.com>
2022-01-07 15:39:10 +0000

    change to the method that will be adding trainees from the waiting list

--- src/main/java/controller/MonthlyIncrementer.java
+++ src/main/java/controller/MonthlyIncrementer.java
@@ -2,7 +2,6 @@ package controller;
 
 import model.*;
 import model.CentresTypes.CentreType;
-
 import java.util.ArrayList;
 
 public class MonthlyIncrementer {
@@ -15,11 +14,12 @@ public class MonthlyIncrementer {
         setCentreMonthlyCapacity();
 
         //add new trainees to waiting list
-        //waitingList add(getNewTrainees());
+        //TraineeManager.addRandomNumberOfTrainees();
 
-        addTraineesToCentres(); //pass waiting list
+        addWaitingListToCentres(); //pass waiting list
         if (currentMonth >= 3) {
-            checkCentreClosure();
+            //returns
+            CentreManager.attemptCloseCentres();
         }
     }
 
@@ -30,42 +30,49 @@ public class MonthlyIncrementer {
     private static void setCentreMonthlyCapacity() {
         // pass through array list of available centres
         for (CentreType centre: CentreManager.getAvailableCentres()) {
-            centre.setMonthlyCapacity(RandomNumberGenerator.getRandomInt(1,50));
+            centre.setMonthlyCapacity(RandomNumberGenerator.getRandomInt(1,50)); //change 50 to monthly capacity variable
         }
     }
 
-    private static ArrayList<Trainee> getNewTrainees() {
-        return TraineeGenerator.getRandomNumOfTrainees();
+    private static void addWaitingListToCentres() {
+        /*
+        iterator
+        for(Trainee trainee : waitingList) {
+        boolean false
+        int count = 0;
+        loop
+            //generate random number to select the centre
+            CentreType centre = availableCentres.get(RandomNumberGenerator.getRandomInt(0, availableCentres.size()));
+            //check if the centre isn't full, isn't at monthly capacity, and if tech centre it's the same course type as the trainee
+            if(centre.getClass == TechCentre.Class) {
+                TechCentre techCentre = centre;
+                if(trainee.getCourse() == techCentre.getCourse()) {
+                    addTraineeToCentre(trainee, techCentre);
+                    boolean true
+                } else {
+                    count++;
+                }
+            }else {
+                addTraineeToCentre(trainee, centre);
+            }
+        }
+        */
     }
 
-    private static void addTraineesToCentres() { //pass in arraylist of trainees
-        //
+    private static void addTraineeToCentre(Trainee trainee, CentreType centre){
         /*
-        ArrayList<CentreType> availableCentres = CentreManager.getAvailableCentres();
-        for(Trainee trainee : availableCentres) {
-            generate random number to select the centre
-            check if the centre isn't full, isn't at monthly capacity, and if tech centre it's the same course type as the trainee
-                centreTrainees.add(trainee);
-                remove trainee from arrayList
-            else
-                add trainee to waitingList
+        if(!centre.isFull() && !centre.getMonthlyCapacity() == 0) {
+            centreTrainees.add(trainee);
+            remove trainee from arrayList
+        }else {
+            add trainee to waitingList
         }
         */
     }
 
-    private static void checkCentreClosure() {
-        //check centres for closure
+    private static void addTraineesToCentres() { //pass in array list of trainees
         /*
-        for (CentreType centre: CentreManager.getAvailableCentres()) {
-            if(centre.attemptShutCentreDown()) {
-                centre safetyNet--;
-            }
-            if(centre safetyNet == 0) {
-                get arraylist of trainees
-                close centre
-                addTraineesToCentres(arraylist of trainees);
-            }
-        }
-        */
+
+         */
     }
 }
