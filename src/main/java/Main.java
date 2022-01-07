3080d2664fd161162b671185ad3cb834dfeab3ae
Hanif Ali <ali.hanif@outlook.com>
2022-01-07 10:56:47 +0000

    updated spartaSimulator/ main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import controller.SpartaSimulator;
 import view.InputHandler;
 import view.OutputManager;
 
@@ -6,14 +7,9 @@ public class Main {
     public static void main(String[] args) {
         InputHandler.setStartingParameters();
 
-        int months = InputHandler.getMonthsOfSimulation();
-        // The code below is hardcoded temporary data that needs to be replaced by Model/Controller
-        int[] fullCentres = {1, 2, 3, 4, 5, 6, 7};
-        int[] openCentres = {1, 2, 3, 4, 5, 6, 7};
-        int[] closedCentres = {1, 2, 3, 4, 5, 6, 7};
-        int[] currentTrainees = {1, 2, 3, 4, 5};
-        int[] waitingListSize = {1, 2, 3, 4, 5};
+        SpartaSimulator spartaSimulator = new SpartaSimulator();
+        spartaSimulator.simulator();
 
-        OutputManager.outputData(months, fullCentres, openCentres, closedCentres, currentTrainees, waitingListSize);
+
     }
 }
--- src/main/java/controller/SpartaSimulator.java
+++ src/main/java/controller/SpartaSimulator.java
@@ -2,6 +2,7 @@ package controller;
 
 import model.Centre;
 import model.CentreManager;
+import model.Trainee;
 import view.InputHandler;
 import view.OutputManager;
 
@@ -12,27 +13,44 @@ public class SpartaSimulator {
     private CentreManager centreManager = new CentreManager();
 
     private int months ;
-    private int numOfOpenCentres;
-    private int numOfFullCentres;
-    private int numOfTraineesTraining;
+    private boolean displayEveryMonth;
+    private int[] openCenters;
+    private int[] fullCenters;
+    private int[] closedCenter;
+    private int[] traineesTraining;
     //change from integer to trainee
-    private ArrayList<Integer> waitingList = new ArrayList<>();
+    private int[] waitingList;
 
     public SpartaSimulator() {
         this.months = InputHandler.getMonthsOfSimulation();
+        this.displayEveryMonth = InputHandler.getDisplayEveryMonth();
     }
 
     public void simulator(){
-
         for (int i = 0; i < months; i++) {
             MonthlyIncrementer.incrementMonths(i);
+
+            if (displayEveryMonth) {
+                getInformation();
+                OutputManager.outputData(i+1, fullCenters, openCenters, closedCenter, traineesTraining,  waitingList);
+            }
         }
-        this.numOfOpenCentres = centreManager.getAvailableCentres().size();
-        this.numOfFullCentres = centreManager.getFullCentres();
-        this.numOfTraineesTraining = centreManager.getTraineesCurrentlyTraining();
-        //this.waitingList = ;
 
-        OutputManager.outputFinalMonth(months, numOfFullCentres, numOfOpenCentres, numOfTraineesTraining, waitingList.size());
+        //final results
+        if(!displayEveryMonth) {
+            getInformation();
+            OutputManager.outputData(months, fullCenters, openCenters, closedCenter, traineesTraining, waitingList);
+        }
 
+    }
+    private void getInformation(){
+        //change this with getter from model
+        this.openCenters = new int[] {1,2,3,4,5,6,7};
+        this.fullCenters = new int[] {1,2,3,4,5,6,7};
+        this.closedCenter = new int[] {1,2,3,4,5,6,7};
+        this.traineesTraining = new int[] {1,2,3,4,5};
+        this.traineesTraining = new int[] {1,2,3,4,5};
+        this.waitingList = new int[] {1,2,3,4,5};
+        ;
     }
 }
