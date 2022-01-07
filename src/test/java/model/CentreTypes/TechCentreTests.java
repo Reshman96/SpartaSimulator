10ff5f41adec0281427607c6282a1c55e78c4358
Harry Jones <harryrjones@yahoo.com>
2022-01-07 14:12:03 +0000

    Written TechCentreTests

--- src/test/java/model/CentreTypes/TechCentreTests.java
+++ src/test/java/model/CentreTypes/TechCentreTests.java
@@ -2,11 +2,18 @@ package model.CentreTypes;
 
 import model.CentresTypes.TechCentre;
 import model.CourseType;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TechCentreTests {
+
+    TechCentre techCentre;
+
+    @BeforeEach
+    void setup(){
+        techCentre = new TechCentre(CourseType.DATA);
+    }
 
     @Test
     @DisplayName("CanYouCreateMe")
@@ -17,14 +24,30 @@ public class TechCentreTests {
     @Test
     @DisplayName("Return max size")
     void returnMaxSize() {
-        TechCentre techCentre = new TechCentre(CourseType.JAVA);
         Assertions.assertEquals(200, techCentre.getMaxSize());
     }
 
     @Test
     @DisplayName("does getCourseTypeIndex of DATA return 3?")
     void doesGetCourseTypeIndexOfDataReturn3() {
-        TechCentre techCentre = new TechCentre(CourseType.DATA);
         Assertions.assertEquals(4, techCentre.getCourseTypeIndex());
+    }
+
+    @Test
+    @DisplayName("Attempt low attendance shutdown")
+    void attemptLowAttendanceShutdown() {
+        assertTrue(techCentre.attemptShutCentreDown());
+    }
+
+    @Test
+    @DisplayName("Attempt high attendance shutdown")
+    void attemptHighAttendanceShutdown() {
+        techCentre.setNumberOfTrainees(new int[] {10,10,10,10,10});
+        assertFalse(techCentre.attemptShutCentreDown());
+    }
+
+    @AfterEach
+    void tearDown(){
+        techCentre = null;
     }
 }
