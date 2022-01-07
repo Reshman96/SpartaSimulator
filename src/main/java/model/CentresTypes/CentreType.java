f3bd38c3826d180df602abe3b31621a8ac89b626
Harry Jones <harryrjones@yahoo.com>
2022-01-07 14:31:14 +0000

    Refactored Trainee storage to be an arraylist rather than an integer array

--- src/main/java/model/CentresTypes/CentreType.java
+++ src/main/java/model/CentresTypes/CentreType.java
@@ -1,28 +1,54 @@
 package model.CentresTypes;
 
-import java.util.Arrays;
+import model.Trainee;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public abstract class CentreType {
-    private int[] numberOfTrainees;
+    private ArrayList<Trainee> traineeList;
     private int monthlyCapacity;
     private int maxSize;
 
     public CentreType() {
-        this.numberOfTrainees = new int[]{0, 0, 0, 0, 0};
+        traineeList = new ArrayList<>();
         this.monthlyCapacity = 0;
         this.maxSize = getMaxSize();
     }
 
     public int getNumberOfTrainees() {
-        return Arrays.stream(numberOfTrainees).sum();
+        return traineeList.size();
     }
 
-    public int[] getTypesOfTrainees(){ return numberOfTrainees;}
+    public int[] getTypesOfTrainees(){
+        int[] typeCount = {0, 0, 0, 0, 0};
+
+        for (Trainee trainee : traineeList) {
+            switch (trainee.getCourse()) {
+                case JAVA:
+                    typeCount[0]++;
+                case CSHARP:
+                    typeCount[1]++;
+                case DATA:
+                    typeCount[2]++;
+                case DEVOPS:
+                    typeCount[3]++;
+                case BUSINESS:
+                    typeCount[4]++;
+            }
+        }
+
+        return typeCount;
+    }
 
     public abstract int getMaxSize();
 
-    public void setNumberOfTrainees(int[] numberOfTrainees) {
-        this.numberOfTrainees = numberOfTrainees;
+    public void setTraineeList(ArrayList<Trainee> traineeList) {
+        this.traineeList = traineeList;
+    }
+
+    public ArrayList<Trainee> getTraineeList(){
+        return traineeList;
     }
 
     public int getMonthlyCapacity() {
