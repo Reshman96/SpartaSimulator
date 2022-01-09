2d14e9f54748b579f77a6dcdbf5e2c19967ba129
resh <reshmansiva96@gmail.com>
2022-01-09 11:23:32 +0000

    uses the FormattedProperties class instead of Properties class, also breaks up the lines of variables in the generated config file into easy to read chunks

--- src/main/java/view/DefaultConfigMaker.java
+++ src/main/java/view/DefaultConfigMaker.java
@@ -2,13 +2,14 @@ package view;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Properties;
 
 public class DefaultConfigMaker {
 
-    public static void createDefaultConfig() {
-        Properties properties = new Properties();
+    public static void createDefaultConfig() throws IOException {
+        FormattedProperties properties;
+        FileWriter fileWriter = new FileWriter("src/main/resources/config.properties");
         try {
+            properties = new FormattedProperties();
             properties.setProperty("traineeLowerBound", "50");
             properties.setProperty("traineeUpperBound", "100");
             properties.setProperty("centresPerMonth", "0.5");
@@ -17,22 +18,27 @@ public class DefaultConfigMaker {
             properties.setProperty("displayEveryMonth", "false");
             properties.setProperty("centreMonthlyIntakeLowerBound", "1");
             properties.setProperty("centreMonthlyIntakeUpperBound", "50");
+            properties.store(fileWriter, "Default Values");
 
+            properties = new FormattedProperties();
             properties.setProperty("trainingHubMinimumTrainees", "25");
             properties.setProperty("trainingHubMaximumTrainees", "100");
             properties.setProperty("trainingHubConsecutiveMonthsOfInadequateTrainees", "1");
             properties.setProperty("trainingHubsPerMonth", "3");
+            properties.store(fileWriter, "Training Hub");
 
+            properties = new FormattedProperties();
             properties.setProperty("bootcampMinimumTrainees", "25");
             properties.setProperty("bootcampMaximumTrainees", "500");
             properties.setProperty("bootcampConsecutiveMonthsOfInadequateTrainees", "3");
             properties.setProperty("bootcampTotalCreations", "2");
+            properties.store(fileWriter, "Bootcamps");
 
+            properties = new FormattedProperties();
             properties.setProperty("techCentreMinimumTrainees", "25");
             properties.setProperty("techCentreMaximumTrainees", "200");
             properties.setProperty("techCentresConsecutiveMonthsOfInadequateTrainees", "1");
-
-            properties.store(new FileWriter("src/main/resources/config.properties"), null);
+            properties.store(fileWriter, "Tech Centres");
 
         } catch (IOException e) {
             e.printStackTrace();
